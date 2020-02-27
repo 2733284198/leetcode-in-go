08c85467462e065974ef985ddbee3b63d0426ed0
aierui <aieruishi@gmail.com>
2020-02-27 23:58:23 +0800

    add 0092 reverse linked list ii

---
+++ algo/0092.reverse-linked-list-ii/reverse-linked-list-ii.go
@@ -0,0 +1,35 @@
+package problem0092
+
+// ListNode export singy-linked list
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseBetween(head *ListNode, m, n int) *ListNode {
+	if head == nil || m == n {
+		return head
+	}
+
+	prev, i, j := &ListNode{Next: head}, m, n-m
+
+	d := prev
+
+	for i > 1 {
+		d = d.Next
+		i--
+	}
+
+	cur := d.Next.Next
+	curPre := d.Next
+
+	for j > 0 {
+		curPre.Next = cur.Next
+		cur.Next = d.Next
+		d.Next = cur
+		cur = curPre.Next
+		j--
+	}
+
+	return prev.Next
+}
---
+++ algo/0092.reverse-linked-list-ii/reverse-linked-list-ii_test.go
@@ -0,0 +1,89 @@
+package problem0092
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+type para struct {
+	one   []int
+	two   int
+	three int
+}
+
+type ans struct {
+	one []int
+}
+
+func Test_Problem0092(t *testing.T) {
+
+	ast := assert.New(t)
+
+	qs := []question{
+		question{
+			para{
+				[]int{1, 2, 3, 4, 5},
+				2,
+				4,
+			},
+			ans{
+				[]int{1, 4, 3, 2, 5},
+			},
+		},
+		question{
+			para{
+				[]int{1, 2, 3, 4, 5},
+				2,
+				4,
+			},
+			ans{
+				[]int{1, 4, 3, 2, 5},
+			},
+		},
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+		ast.Equal(a.one, l2s(reverseBetween(s2l(p.one), p.two, p.three)), "输入:%v", p)
+	}
+}
+
+// convert *ListNode to []int
+func l2s(head *ListNode) []int {
+	res := []int{}
+
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
+
+// convert []int to *ListNode
+func s2l(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	res := &ListNode{
+		Val: nums[0],
+	}
+	temp := res
+	for i := 1; i < len(nums); i++ {
+		temp.Next = &ListNode{
+			Val: nums[i],
+		}
+		temp = temp.Next
+	}
+
+	return res
+}
