55066554625301ee0e6cfab2ffce0d7d7d0e4ed0
aierui <aieruishi@gmail.com>
2020-03-07 23:49:47 +0800

    add four num

---
+++ algo/0018.four-sum/four-sum.go
@@ -0,0 +1,5 @@
+package problem0018
+
+func fourSum(nums []int, target int) [][]int {
+
+}
---
+++ algo/0018.four-sum/four-sum_test.go
@@ -0,0 +1,12 @@
+package problem0018
+
+
+func Test_FourSum(t *testing.T) {
+	
+}
+
+func Benchmark_FourSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		
+	}
+}
