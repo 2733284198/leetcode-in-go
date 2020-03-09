3102d971bca97bbd641303283e5b97c5c12677bf
aierui <aieruishi@gmail.com>
2020-03-09 23:28:17 +0800

    add 0088 merge sorted array solutaion

--- algo/0088.merge-sorted-array/merge-sorted-array.go
+++ algo/0088.merge-sorted-array/merge-sorted-array.go
@@ -1,5 +1,8 @@
 package problem0088
 
-func mergeSortedArray(nums1 []int, m int, nums2 []int) {
+import "sort"
 
+func mergeSortedArray(nums1 []int, m int, nums2 []int, n int) {
+	nums1 = append(nums1[:m], nums2[:n]...)
+	sort.Ints(nums1) // 依赖标准库
 }
--- algo/0088.merge-sorted-array/merge-sorted-array_test.go
+++ algo/0088.merge-sorted-array/merge-sorted-array_test.go
@@ -1,8 +1,37 @@
 package problem0088
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 int
+	para3 []int
+	para4 int
+
+	ans []int
+}{
+	{
+		[]int{1, 2, 3, 0, 0, 0},
+		3,
+		[]int{2, 5, 6},
+		3,
+		[]int{1, 2, 2, 3, 5, 6},
+	},
+}
 
 func Test_MergeSortedArray(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		mergeSortedArray(q.para1, q.para2, q.para3, q.para4)
+		ast.Equal(q.ans, q.para1, "输入：%v", q)
+	}
 
 }
 
