462b3d9ff117ddb9ff1ef4451c2f2099e05f92d9
aierui <aieruishi@gmail.com>
2020-03-10 23:44:23 +0800

    add 0155 min stack solution

--- algo/0155.min-stack/min-stack.go
+++ algo/0155.min-stack/min-stack.go
@@ -1,35 +1,38 @@
 package problem0155
 
 type MinStack struct {
-
+	elements []int
+	h        []int
 }
-
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
-
+	return MinStack{}
 }
 
-
-func (this *MinStack) Push(x int)  {
-
+func (this *MinStack) Push(x int) {
+	this.elements = append(this.elements, x)
+	if len(this.h) == 0 {
+		this.h = append(this.h, x)
+	} else if this.h[len(this.h)-1] < x {
+		this.h = append(this.h, this.h[len(this.h)-1])
+	} else {
+		this.h = append(this.h, x)
+	}
 }
 
-
-func (this *MinStack) Pop()  {
-
+func (this *MinStack) Pop() {
+	this.elements = this.elements[:len(this.elements)-1]
+	this.h = this.h[:len(this.h)-1]
 }
-
 
 func (this *MinStack) Top() int {
-
+	return this.elements[len(this.elements)-1]
 }
-
 
 func (this *MinStack) GetMin() int {
-
+	return this.h[len(this.h)-1]
 }
-
 
 /**
  * Your MinStack object will be instantiated and called as such:
--- algo/0155.min-stack/min-stack_test.go
+++ algo/0155.min-stack/min-stack_test.go
@@ -1,13 +1,28 @@
 package problem0155
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test(t *testing.T) {
+	ast := assert.New(t)
 
-}
+	s := Constructor()
 
-func Benchmark(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-
-	}
+	s.Push(-2)
+	// [-2]
+	s.Push(0)
+	// [-2, 0]
+	s.Push(-3)
+	// [-2, 0, -3]
+	ast.Equal(-3, s.GetMin(), "get min from [-2, 0, -3]")
+	// [-2, 0, -3]
+	s.Pop()
+	// [-2, 0]
+	ast.Equal(0, s.Top(), "get top from [-2, 0]")
+	// [-2, 0]
+	ast.Equal(-2, s.GetMin(), "get min from [-2, 0]")
+	// [-2, 0]
 }
