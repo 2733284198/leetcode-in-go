63540c6ff610a1f8c0e3e95ee342a4c3a5c07d7d
aierui <aieruishi@gmail.com>
2020-03-15 23:03:30 +0800

    0047 permutations ii

--- algo/0047.permutations-ii/permutations-ii.go
+++ algo/0047.permutations-ii/permutations-ii.go
@@ -1,5 +1,25 @@
 package problem0047
 
 func permuteUnique(nums []int) [][]int {
+	res := [][]int{}
+	backTrace(0, nums, &res)
+	return res
+}
 
+func backTrace(first int, nums []int, res *[][]int) {
+	if first == len(nums) {
+		temp := make([]int, len(nums))
+		copy(temp, nums)
+		*res = append(*res, temp)
+	}
+	m := map[int]bool{}
+	for i := first; i < len(nums); i++ {
+		if m[nums[i]] {
+			continue
+		}
+		m[nums[i]] = true
+		nums[first], nums[i] = nums[i], nums[first]
+		backTrace(first+1, nums, res)
+		nums[first], nums[i] = nums[i], nums[first]
+	}
 }
--- algo/0047.permutations-ii/permutations-ii_test.go
+++ algo/0047.permutations-ii/permutations-ii_test.go
@@ -1,5 +1,31 @@
 package problem0047
 
-func Test(t *testing.T) {
-	
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  [][]int
+}{
+	{
+		[]int{1, 1, 2},
+		[][]int{
+			[]int{1, 1, 2},
+			[]int{1, 2, 1},
+			[]int{2, 1, 1},
+		},
+	},
+}
+
+func Test_PermuteUnique(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, permuteUnique(q.para), "输入：%v\n", q)
+	}
 }
