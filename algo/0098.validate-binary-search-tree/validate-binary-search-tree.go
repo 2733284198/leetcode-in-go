0f0e084bcc5b45c462c529a8b090ed3f9a2a5303
aierui <aieruishi@gmail.com>
2020-03-18 23:33:58 +0800

    add 0098 solutions

--- algo/0098.validate-binary-search-tree/validate-binary-search-tree.go
+++ algo/0098.validate-binary-search-tree/validate-binary-search-tree.go
@@ -1,10 +1,30 @@
-package prolbem0098
+package problem0098
 
-import "github.com/aierui/leetcode-in-go/kit"
+import (
+	"math"
+
+	"github.com/aierui/leetcode-in-go/kit"
+)
 
 type TreeNode = kit.TreeNode
-
+// isValidBST by recursion
 func isValidBST(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return isBST(root, math.MinInt64, math.MaxInt64)
+}
+
+func isBST(root *TreeNode, left, right int) bool  {
 	
-	
+	if root == nil {
+		return true
+	}
+
+	if left >= root.Val || right <= root.Val {
+		return false
+	}
+
+	return isBST(root.Left, left, root.Val) && isBST(root.Right, root.Val, right)
 }
--- algo/0098.validate-binary-search-tree/validate-binary-search-tree_test.go
+++ algo/0098.validate-binary-search-tree/validate-binary-search-tree_test.go
@@ -1,13 +1,34 @@
 package problem0098
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func Test(t *testing.T) {
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
 
+var qs = []struct {
+	para1 []int
+	para2 []int
+	ans   bool
+}{
+	{
+		[]int{2, 1, 3},
+		[]int{1, 2, 3},
+		true,
+	},
+	{
+		[]int{5, 1, 4, 3, 6},
+		[]int{1, 5, 3, 4, 6},
+		false,
+	},
 }
 
-func Benchmark(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-
+func Test(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, isValidBST(kit.PreIn2Tree(q.para1, q.para2)), "输入：%v\n", q)
 	}
 }
