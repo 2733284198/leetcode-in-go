73688e103f67fd6a6db00929dcd654fd913a8eb3
aierui <aieruishi@gmail.com>
2020-03-24 00:29:21 +0800

    add 0055 jump game solution

--- algo/0055.jump-game/jump-game.go
+++ algo/0055.jump-game/jump-game.go
@@ -1,6 +1,20 @@
 package problem0055
 
 func canJump(nums []int) bool {
+	max := 0
 
-	return false
+	// 遍历数组，得到当前位置最长能跳多远max，碰到0看看max能跳过去不，不能跳过去就是false，注意边界条件。
+	for index, n := range nums {
+		if n == 0 && index != len(nums)-1 {
+			if max <= 1 {
+				return false
+			}
+		}
+		max = max - 1
+		if n > max {
+			max = n
+		}
+	}
+
+	return true
 }
