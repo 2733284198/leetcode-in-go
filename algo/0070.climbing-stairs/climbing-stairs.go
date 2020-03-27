fe54c39c59f39a863d7bd857de60f4b2dd6ad187
aierui <aieruishi@gmail.com>
2020-03-28 00:09:41 +0800

    add 0070 climbing stairs solution

--- algo/0070.climbing-stairs/climbing-stairs.go
+++ algo/0070.climbing-stairs/climbing-stairs.go
@@ -1,5 +1,50 @@
 package problem0070
 
 func climbStairs(n int) int {
-	
+	if n == 1 {
+		return 1
+	}
+
+	var first, second, third = 1, 2, 0
+
+	for i := 3; i <= n; i++ {
+		third = first + second
+		first = second
+		second = third
+	}
+
+	return second
+}
+
+func climbStairsByBF(n int) int {
+	return climbBruteForce(0, n)
+}
+
+func climbBruteForce(i, n int) int  { // i 定义了当前阶数，n 定义了目标阶数
+	if i > n {
+		return 0
+	}
+
+	if i == n {
+		return 1
+	}
+
+	return climbBruteForce(i + 1, n) + climbBruteForce(i + 2, n)
+}
+
+func climbStairsDp(n int) int{
+	if n == 1 {
+		return 1
+	}
+
+	dp := make([]int, n + 1)
+
+	dp[1] = 1
+	dp[2] = 2
+
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
 }
--- algo/0070.climbing-stairs/climbing-stairs_test.go
+++ algo/0070.climbing-stairs/climbing-stairs_test.go
@@ -19,6 +19,10 @@ var qs = []struct {
 		3,
 		3,
 	},
+	{
+		10,
+		89,
+	},
 }
 
 func Test_ClimbStairs(t *testing.T) {
@@ -26,5 +30,23 @@ func Test_ClimbStairs(t *testing.T) {
 	for _, q := range qs {
 		fmt.Printf("~~%v~~\n", q)
 		ast.Equal(q.ans, climbStairs(q.para), "输入：%v\n", q)
+	}
+}
+
+
+func Test_ClimbStairsByBF(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, climbStairsByBF(q.para), "输入：%v\n", q)
+	}
+}
+
+
+func Test_ClimbStairsDp(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, climbStairsDp(q.para), "输入：%v\n", q)
 	}
 }
