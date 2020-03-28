c032feb694aa4ff080d60793637cac4fd9d5f9d0
aierui <aieruishi@gmail.com>
2020-03-28 13:52:16 +0800

    add 0071 simplify path

--- algo/0071.simplify-path/simplify-path_test.go
+++ algo/0071.simplify-path/simplify-path_test.go
@@ -1,26 +1,31 @@
 package problem0071
 
-import "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var qs = []struct {
 	para string
 	ans  string
-}{ 
+}{
 	{
 		"/home/",
 		"/home",
 	},
 	{
 		"/../",
-		"/"
+		"/",
 	},
 	{
 		"/home//foo",
-		"/home/foo"
+		"/home/foo",
 	},
 	{
 		"/a/./b/../../c/",
-		"/c"
+		"/c",
 	},
 	{
 		"/a/../../b/../c//.//",
@@ -32,11 +37,10 @@ var qs = []struct {
 	},
 }
 
-
 func Test_SimplifyPath(t *testing.T) {
 	ast := assert.New(t)
 	for _, q := range qs {
-		fmt.Printf("~~%v~~\n",q)
+		fmt.Printf("~~%v~~\n", q)
 		ast.Equal(q.ans, simplifyPath(q.para), "输入：%v\n", q)
 	}
 }
