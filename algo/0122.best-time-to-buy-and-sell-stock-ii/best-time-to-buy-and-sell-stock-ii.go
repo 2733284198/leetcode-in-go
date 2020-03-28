173aa65dd24c6862f93cbb911c9940d6050c026e
aierui <aieruishi@gmail.com>
2020-03-28 23:51:39 +0800

    add 0122 best time to buy and sell stock ii

--- algo/0122.best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
+++ algo/0122.best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
@@ -1,5 +1,14 @@
 package problem0122
 
 func maxProfit(prices []int) int {
+	// i j (j>i)
+	max := 0
 
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-prices[i-1] > 0 {
+			max += prices[i] - prices[i-1]
+		}
+	}
+
+	return max
 }
