6556fba68010bf71492b3178d3ae5426cc22e0fa
aierui <aieruishi@gmail.com>
2020-03-29 00:21:52 +0800

    add 0123 best time to buy and sell stock iii

---
+++ algo/0123.best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
@@ -0,0 +1,5 @@
+package problem0123
+
+func maxProfit(prices []int) int {
+
+}
---
+++ algo/0123.best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii_test.go
@@ -0,0 +1,46 @@
+package problem0123
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  int
+}{
+	{
+		[]int{1, 2, 3, 4, 5},
+		4,
+	},
+	{
+		[]int{7, 6, 5, 4, 3, 2},
+		0,
+	},
+	{
+		[]int{7, 1, 5, 3, 6, 4},
+		7,
+	},
+	{
+		[]int{7, 2, 5, 3, 2, 1},
+		3,
+	},
+	{
+		[]int{7, 3, 5, 3, 1, 2},
+		3,
+	},
+	{
+		[]int{7, 1, 5, 7, 2, 2},
+		6,
+	},
+}
+
+func Test_MaxProfit(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, maxProfit(q.para), "输入：%v\n", q)
+	}
+}
