4a69946a4b5a80e75a734dff247f265a8f15dd9c
aierui <aieruishi@gmail.com>
2020-03-29 23:36:40 +0800

    add 0188 best time to buy and sell stock iv solution

--- algo/0188.best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv_test.go
+++ algo/0188.best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv_test.go
@@ -8,36 +8,44 @@ import (
 )
 
 var qs = []struct {
-	para []int
-	ans  int
+	para1 []int
+	para2 int
+	ans   int
 }{
 	{
 		[]int{1, 2, 3, 4, 5},
+		2,
 		4,
 	},
 	{
 		[]int{7, 6, 5, 4, 3, 2},
+		3,
 		0,
 	},
 	{
 		[]int{7, 1, 5, 3, 6, 4},
+		4,
 		7,
 	},
 	{
 		[]int{7, 2, 5, 3, 2, 1},
+		1,
 		3,
 	},
 	{
 		[]int{7, 3, 5, 3, 1, 2},
 		3,
+		3,
 	},
 	{
 		[]int{7, 1, 5, 7, 2, 2},
+		4,
 		6,
 	},
 	{
-		[]int{2, 1, 5, 7, 1, 9},
-		14,
+		[]int{2, 1, 5, 7, 1, 9, 1, 1, 31, 10, 100, 1, 19},
+		6,
+		152,
 	},
 }
 
@@ -45,14 +53,6 @@ func Test_MaxProfit(t *testing.T) {
 	ast := assert.New(t)
 	for _, q := range qs {
 		fmt.Printf("~~%v~~\n", q)
-		ast.Equal(q.ans, maxProfit(q.para), "输入：%v\n", q)
-	}
-}
-
-func Benchmark_MaxProfit(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, q := range qs {
-			maxProfit(q.para)
-		}
+		ast.Equal(q.ans, maxProfit(q.para2, q.para1), "输入：%v\n", q)
 	}
 }
