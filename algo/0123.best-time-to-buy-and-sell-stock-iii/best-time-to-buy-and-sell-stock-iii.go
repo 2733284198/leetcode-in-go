9011eb768fd155cfa7f973af99643447413f1751
aierui <aieruishi@gmail.com>
2020-03-29 22:41:52 +0800

    add 0123 best time to buy and sell stock iii solution

--- algo/0123.best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
+++ algo/0123.best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
@@ -1,5 +1,107 @@
 package problem0123
 
 func maxProfit(prices []int) int {
+	size := len(prices)
+	if size <= 1 {
+		return 0
+	}
 
+	maxK := 2 // max trade nums
+
+	dp := make([][][]int, size)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([][]int, maxK+1)
+		for j := 0; j <= maxK; j++ {
+			dp[i][j] = make([]int, 2)
+		}
+	}
+
+	// 第 0 天 持有股票
+	dp[0][0][1] = -prices[0]
+	// 第 0 天持有股票，交易 1 比
+	dp[0][1][1] = -prices[0]
+	// 第 0 天持有股票，交易 2 比
+	dp[0][2][1] = -prices[0]
+
+	for i := 1; i < size; i++ {
+		for k := 1; k <= maxK; k++ {
+			dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1]+prices[i])
+			dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i])
+		}
+	}
+	return dp[size-1][maxK][0]
 }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxProfitV2(prices []int) int {
+	size := len(prices)
+	if size <= 1 {
+		return 0
+	}
+
+	profits := []int{}
+	temp := 0
+	for i := 1; i < size; i++ {
+		diff := prices[i] - prices[i-1]
+
+		if temp*diff >= 0 {
+			temp += diff
+			continue
+		}
+
+		profits = append(profits, temp)
+		temp = diff
+	}
+	profits = append(profits, temp)
+
+	res := 0
+	for i := 0; i < len(profits); i++ {
+		temp = maxSliceEle(profits[:i]) + maxSliceEle(profits[i:])
+		if res < temp {
+			res = temp
+		}
+	}
+
+	return res
+}
+
+func maxSliceEle(ps []int) int {
+	max, tmp := 0, 0
+
+	for _, p := range ps {
+		if tmp < 0 {
+			tmp = 0
+		}
+
+		tmp += p
+
+		if max < tmp {
+			max = tmp
+		}
+	}
+
+	return max
+}
+
+// func maxProfitV3(prices []int) int {
+// 	if len(prices) <= 1 {
+// 		return 0
+// 	}
+
+// 	s1, s2, s3, s4 := prices[0], math.MinInt32, math.MinInt32, math.MinInt32
+
+// 	for i := 1; i < len(prices); i++ {
+// 		s1 = max(s1, -prices[i])
+// 		s2 = max(s2, s1+prices[i])
+// 		s3 = max(s3, s2-prices[i])
+// 		s4 = max(s4, s3+prices[i])
+// 	}
+
+// 	return max(0, s4)
+// }
--- algo/0123.best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii_test.go
+++ algo/0123.best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii_test.go
@@ -35,6 +35,10 @@ var qs = []struct {
 		[]int{7, 1, 5, 7, 2, 2},
 		6,
 	},
+	{
+		[]int{2, 1, 5, 7, 1, 9},
+		14,
+	},
 }
 
 func Test_MaxProfit(t *testing.T) {
@@ -42,5 +46,37 @@ func Test_MaxProfit(t *testing.T) {
 	for _, q := range qs {
 		fmt.Printf("~~%v~~\n", q)
 		ast.Equal(q.ans, maxProfit(q.para), "输入：%v\n", q)
+	}
+}
+
+func Test_MaxProfitV2(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, maxProfitV2(q.para), "输入：%v\n", q)
+	}
+}
+
+// func Test_MaxProfitV3(t *testing.T) {
+// 	ast := assert.New(t)
+// 	for _, q := range qs {
+// 		fmt.Printf("~~%v~~\n", q)
+// 		ast.Equal(q.ans, maxProfitV3(q.para), "输入：%v\n", q)
+// 	}
+// }
+
+func Benchmark_MaxProfit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs {
+			maxProfit(q.para)
+		}
+	}
+}
+
+func Benchmark_MaxProfitV2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs {
+			maxProfitV2(q.para)
+		}
 	}
 }
