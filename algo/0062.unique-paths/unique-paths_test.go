ef82b08bc757e0d31aaf3fb466b0c37287508995
aierui <aieruishi@gmail.com>
2020-04-01 23:29:08 +0800

    add 0062 unique paths solutions

--- algo/0062.unique-paths/unique-paths_test.go
+++ algo/0062.unique-paths/unique-paths_test.go
@@ -1,7 +1,30 @@
 package problem0062
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1, para2 int
+	ans          int
+}{
+	{
+		3, 2,
+		3,
+	},
+	{
+		7, 3,
+		28,
+	},
+}
 
 func Test_UniquePaths(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, uniquePaths(q.para1, q.para2), "输入：%v", q)
+	}
 }
