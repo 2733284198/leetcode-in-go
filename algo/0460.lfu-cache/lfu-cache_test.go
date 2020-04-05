76117f14c72afb8694d3dcc6ee1ac3698d3f3d58
aierui <aieruishi@gmail.com>
2020-04-05 18:53:52 +0800

    add 0460 lfu cache solution

--- algo/0460.lfu-cache/lfu-cache_test.go
+++ algo/0460.lfu-cache/lfu-cache_test.go
@@ -6,23 +6,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLRUCache_1(t *testing.T) {
+func Test_LFUCache(t *testing.T) {
+	ast := assert.New(t)
+
 	cache := Constructor(2)
+
 	cache.Put(1, 1)
 	cache.Put(2, 2)
-	assert.Equal(t, 1, cache.Get(1)) // returns 1
 
-	cache.Put(3, 3)                   // evicts key 2
-	assert.Equal(t, -1, cache.Get(2)) // returns -1 (not found)
+	ast.Equal(1, cache.Get(1))
 
-	cache.Put(4, 4)                   // evicts key 1
-	assert.Equal(t, -1, cache.Get(1)) // returns -1 (not found)
-	assert.Equal(t, 3, cache.Get(3))  // returns 3
-	assert.Equal(t, 4, cache.Get(4))  // returns 4
+	cache.Put(3, 3)
+
+	ast.Equal(-1, cache.Get(2))
+
+	ast.Equal(3, cache.Get(3))
+
+	cache.Put(4, 4)
+
+	ast.Equal(-1, cache.Get(1))
+
+	ast.Equal(3, cache.Get(3))
+
+	ast.Equal(4, cache.Get(4))
+
 }
 
-func TestLRUCache_2(t *testing.T) {
-	cache := Constructor(1)
+func Test_LFUCache2(t *testing.T) {
+	ast := assert.New(t)
+
+	cache := Constructor(0)
+
+	cache.Put(0, 0)
+
+	ast.Equal(-1, cache.Get(0), "没能正确处理好， cap <= 0 的情况")
+}
+
+func Test_LFUCache3(t *testing.T) {
+	ast := assert.New(t)
+
+	cache := Constructor(2)
+
+	cache.Put(3, 1)
 	cache.Put(2, 1)
-	assert.Equal(t, 1, cache.Get(2))
+	cache.Put(2, 2)
+	cache.Put(4, 4)
+
+	ast.Equal(2, cache.Get(2), "更新 2 的值后，其 frequence 没有增加")
 }
