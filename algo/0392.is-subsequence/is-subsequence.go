d4cea39e8f4facb5eb6d1cb13baadfc401e8d741
aierui <aieruishi@gmail.com>
2020-04-06 18:53:27 +0800

    add 0392 is subsequence solution

--- algo/0392.is-subsequence/is-subsequence.go
+++ algo/0392.is-subsequence/is-subsequence.go
@@ -1,5 +1,17 @@
 package problem0392
 
 func isSubsequence(s, t string) bool {
+	if len(s) > len(t) {
+		return false
+	}
+	t1, t2 := 0, 0
+
+	for t1 < len(s) && t2 < len(t) {
+		if s[t1] == t[t2] {
+			t1++
+		}
+		t2++
+	}
 	
+	return t1 == len(s)
 }
--- algo/0392.is-subsequence/is-subsequence_test.go
+++ algo/0392.is-subsequence/is-subsequence_test.go
@@ -1,7 +1,32 @@
 package problem0392
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1, para2 string
+	ans          bool
+}{
+	{
+		"abc",
+		"ahbgdc",
+		true,
+	},
+	{
+		"axc",
+		"ahbgdc",
+		false,
+	},
+}
 
 func Test_IsSubsequence(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, isSubsequence(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
