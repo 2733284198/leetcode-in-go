eedb421948c1d28a984c786dce2c452816256cb7
aierui <aieruishi@gmail.com>
2020-04-06 13:27:13 +0800

    add 0072 edit distance solution

--- algo/0072.edit-distance/edit-distance_test.go
+++ algo/0072.edit-distance/edit-distance_test.go
@@ -1,7 +1,33 @@
 package problem0072
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 string
+	para2 string
+	ans   int
+}{
+	{
+		"horse",
+		"ros",
+		3,
+	},
+	{
+		"intention",
+		"execution",
+		5,
+	},
+}
 
 func Test_MinDistance(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, minDistance(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
