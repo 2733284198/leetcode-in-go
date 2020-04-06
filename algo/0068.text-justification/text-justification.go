6db719bcd857cca454c4df03ad3accdf7a10d07f
aierui <aieruishi@gmail.com>
2020-04-06 14:58:45 +0800

    add algo 0069 text justification solution

--- algo/0068.text-justification/text-justification.go
+++ algo/0068.text-justification/text-justification.go
@@ -1,5 +1,64 @@
 package problem0068
 
+import "strings"
+
 func fullJustify(words []string, maxWidth int) []string {
-	
+	l := len(words)
+
+	r := []string{}
+	w := []string{}
+	t := 0
+
+	for i := 0; i < l; i++ {
+		t = t + len(words[i])
+		w = append(w, words[i])
+
+		// has words left
+		if i+1 < l {
+			// words-size + spaces-size + next-word-size
+			if t+len(w)+len(words[i+1]) > maxWidth {
+				r = append(r, typeSetting(w, t, maxWidth, false))
+				t = 0
+				w = []string{}
+			}
+		} else {
+			r = append(r, typeSetting(w, t, maxWidth, true))
+		}
+	}
+
+	return r
+}
+
+func typeSetting(words []string, t int, maxWidth int, end bool) string {
+	if end {
+		endLine := strings.Join(words, " ")
+		return endLine + spaces(maxWidth-len(endLine))
+	}
+
+	if len(words) == 1 {
+		return words[0] + spaces(maxWidth-t)
+	}
+
+	spacesCount := len(words) - 1
+	spacesLength := int((maxWidth - t) / spacesCount)
+	tails := (maxWidth - t) % spacesCount
+
+	for i := 0; i < tails; i++ {
+		words[i] = words[i] + " "
+	}
+
+	s := spaces(spacesLength)
+
+	return strings.Join(words, s)
+}
+
+func spaces(n int) string {
+	r := ""
+
+	for n > 0 {
+		r += " "
+		n--
+	}
+
+	return r
 }
--- algo/0068.text-justification/text-justification_test.go
+++ algo/0068.text-justification/text-justification_test.go
@@ -1,6 +1,53 @@
 package problem0068
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []string
+	para2 int
+	ans   []string
+}{
+	{
+		[]string{"This", "is", "an", "example", "of", "text", "justification."},
+		16,
+		[]string{
+			"This    is    an",
+			"example  of text",
+			"justification.  ",
+		},
+	},
+	{
+		[]string{"What", "must", "be", "acknowledgment", "shall", "be"},
+		16,
+		[]string{
+			"What   must   be",
+			"acknowledgment  ",
+			"shall be        ",
+		},
+	},
+	{
+		[]string{"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"},
+		20,
+		[]string{
+			"Science  is  what we",
+			"understand      well",
+			"enough to explain to",
+			"a  computer.  Art is",
+			"everything  else  we",
+			"do                  ",
+		},
+	},
+}
 
 func Test_FullJustify(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, fullJustify(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
