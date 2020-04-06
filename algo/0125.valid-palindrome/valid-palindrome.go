c77e06d316d31ed5b1742b02702a5d40d653d6b8
aierui <aieruishi@gmail.com>
2020-04-06 15:56:37 +0800

    add 0125 valid palindrome solution

--- algo/0125.valid-palindrome/valid-palindrome.go
+++ algo/0125.valid-palindrome/valid-palindrome.go
@@ -1,5 +1,25 @@
 package prolbem0125
 
-func isPalindrome(s string) bool  {
-	
+import "strings"
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	l, r := 0, len(s)-1
+	for l < r {
+		if !('a' <= s[l] && s[l] <= 'z' || '0' <= s[l] && s[l] <= '9') { // 左边 只处理字母和数字
+			l++
+			continue
+		}
+		if !('a' <= s[r] && s[r] <= 'z' || '0' <= s[r] && s[r] <= '9') {// 右边 只处理字母和数字
+			r--
+			continue
+		}
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+	}
+
+	return true
 }
--- algo/0125.valid-palindrome/valid-palindrome_test.go
+++ algo/0125.valid-palindrome/valid-palindrome_test.go
@@ -1,7 +1,30 @@
 package prolbem0125
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para string
+	ans  bool
+}{
+	{
+		"A man, a plan, a canal: Panama",
+		true,
+	},
+	{
+		"race a car",
+		false,
+	},
+}
 
 func Test_IsPalindrome(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, isPalindrome(q.para), "输入：%v\n", q)
+	}
 }
