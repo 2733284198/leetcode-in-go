4abd9aaf6fce8e6bf248dd8af99951ada3ec5851
aierui <aieruishi@gmail.com>
2020-04-06 19:29:55 +0800

    updae 1143 longest common subsequence solution

--- algo/1143.longest-common-subsequence/longest-common-subsequence.go
+++ algo/1143.longest-common-subsequence/longest-common-subsequence.go
@@ -1,5 +1,50 @@
 package problem1143
 
+// longestCommonSubsequence two dimensional
 func longestCommonSubsequence(text1, text2 string) int {
+	// init dp table
+	dp := make([][]int, len(text1)+1)
+	for i := 0; i <= len(text1); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
 
+	for i := 1; i <= len(text1); i++ {
+		for j := 1; j <= len(text2); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[len(text1)][len(text2)]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// longestCommonSubsequenceV2 one dimensional
+func longestCommonSubsequenceV2(text1, text2 string) int {
+	// init dp table
+	dp := make([]int, len(text2)+1)
+
+	for i := 1; i <= len(text1); i++ {
+		last := 0
+		for j := 1; j <= len(text2); j++ {
+			tmp := dp[j] // tmp 记录的是二维dp矩阵正上方的值
+			if text1[i-1] == text2[j-1] {
+				dp[j] = last + 1 // last 记录的是二维dp矩阵左上方的值
+			} else {
+				dp[j] = max(tmp, dp[j-1])
+			}
+			last = tmp
+		}
+	}
+
+	return dp[len(text2)]
 }
--- algo/1143.longest-common-subsequence/longest-common-subsequence_test.go
+++ algo/1143.longest-common-subsequence/longest-common-subsequence_test.go
@@ -1,7 +1,45 @@
 package problem1143
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func Test(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
+var qs = []struct {
+	para1, para2 string
+	ans          int
+}{
+	{
+		"abcde",
+		"ace",
+		3,
+	},
+	{
+		"abc",
+		"abc",
+		3,
+	},
+	{
+		"abc",
+		"def",
+		0,
+	},
+}
+
+func Test_LongestCommonSubsequence(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, longestCommonSubsequence(q.para1, q.para2), "输入：%v\n", q)
+	}
+}
+
+func Test_LongestCommonSubsequenceV2(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, longestCommonSubsequenceV2(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
