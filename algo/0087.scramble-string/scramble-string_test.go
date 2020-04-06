c3e0572476707461a9fc3a6b28b19e0f026d88a7
aierui <aieruishi@gmail.com>
2020-04-06 15:27:00 +0800

    add 0087 scramble string solution

--- algo/0087.scramble-string/scramble-string_test.go
+++ algo/0087.scramble-string/scramble-string_test.go
@@ -1,5 +1,32 @@
 package problem0087
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1, para2 string
+	ans          bool
+}{
+	{
+		"great",
+		"rgeat",
+		true,
+	},
+	{
+		"abcde",
+		"caebd",
+		false,
+	},
+}
+
 func Test_IsScramble(t *testing.T) {
-	
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, isScramble(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
