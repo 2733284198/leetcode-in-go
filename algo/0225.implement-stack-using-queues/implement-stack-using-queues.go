7efbb5dc1315590e11f35d80313c6c3d6982af0b
aierui <aieruishi@gmail.com>
2020-04-07 09:39:22 +0800

    update 0225 implement stack using queues solution

--- algo/0225.implement-stack-using-queues/implement-stack-using-queues.go
+++ algo/0225.implement-stack-using-queues/implement-stack-using-queues.go
@@ -1,31 +1,46 @@
 package problem0225
 
 type MyStack struct {
+	Element []int
 }
 
 /** Initialize your data structure here. */
 func Constructor() MyStack {
+	var myStack MyStack
 
+	return myStack
 }
 
 /** Push element x onto stack. */
 func (this *MyStack) Push(x int) {
+	this.Element = append(this.Element, x)
+
+	n := len(this.Element)
+
+	for i := 0; i < n-1; i++ { // reverse element
+		first := this.Element[0]
+		this.Element = this.Element[1:]
+		this.Element = append(this.Element, first)
+	}
 
 }
 
 /** Removes the element on top of the stack and returns that element. */
 func (this *MyStack) Pop() int {
+	tmp := this.Element[0]
+	this.Element = this.Element[1:]
 
+	return tmp
 }
 
 /** Get the top element. */
 func (this *MyStack) Top() int {
-
+	return this.Element[0]
 }
 
 /** Returns whether the stack is empty. */
 func (this *MyStack) Empty() bool {
-
+	return len(this.Element) == 0
 }
 
 /**
--- algo/0225.implement-stack-using-queues/implement-stack-using-queues_test.go
+++ algo/0225.implement-stack-using-queues/implement-stack-using-queues_test.go
@@ -1,7 +1,27 @@
 package problem0225
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_1(t *testing.T) {
+	ast := assert.New(t)
 
+	s := Constructor()
+	ast.True(s.Empty(), "检查新建的 s 是否为空")
+
+	start, end := 0, 10
+
+	for i := start; i < end; i++ {
+		s.Push(i)
+		ast.Equal(i, s.Top(), "查看 s.Top()")
+	}
+
+	for i := end - 1; i >= start; i-- {
+		ast.Equal(i, s.Pop(), "从 s 中 pop 出数来。")
+	}
+
+	ast.True(s.Empty(), "检查 Pop 完毕后的 s 是否为空")
 }
