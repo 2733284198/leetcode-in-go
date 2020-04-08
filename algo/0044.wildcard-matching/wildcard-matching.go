9f7117b0b2df5d855407d55bc9ffdb7029f0e5c2
aierui <aieruishi@gmail.com>
2020-04-08 09:23:37 +0800

    update 0044 wildcard matching solution

--- algo/0044.wildcard-matching/wildcard-matching.go
+++ algo/0044.wildcard-matching/wildcard-matching.go
@@ -1,6 +1,39 @@
 package problem0044
 
-func isMatch(s, p string) bool {
-
-	return false
+func isMatch(s string, p string) bool {
+	dp := make([][]bool, 0, len(s)+1)
+	for i := 0; i < len(s)+1; i++ {
+		tmp := make([]bool, len(p)+1)
+		dp = append(dp, tmp)
+	}
+	dp[0][0] = true
+	for i, _ := range s {
+		dp[i+1][0] = false
+	}
+	for i, ch := range p {
+		if dp[0][i] && ch == '*' {
+			dp[0][i+1] = true
+		} else {
+			dp[0][i+1] = false
+		}
+	}
+	for i, chS := range s {
+		for j, chP := range p {
+			x, y := i+1, j+1
+			if chP != '*' {
+				if dp[x-1][y-1] && (chP == '?' || chP == chS) {
+					dp[x][y] = true
+				} else {
+					dp[x][y] = false
+				}
+			} else {
+				if dp[x][y-1] || dp[x-1][y] {
+					dp[x][y] = true
+				} else {
+					dp[x][y] = false
+				}
+			}
+		}
+	}
+	return dp[len(s)][len(p)]
 }
