c58761ba6f445378dc2ba5600ca49a72ecadc70b
aierui <aieruishi@gmail.com>
2020-04-08 10:05:39 +0800

    update problems 0402 longest common substring solution

--- algo/problems/0402.longest-common-substring/longest-common-substring.go
+++ algo/problems/0402.longest-common-substring/longest-common-substring.go
@@ -38,3 +38,34 @@ func maxSubTwoString(str1, str2 string) string {
 
 	return str1[(end - maxLength + 1):(end + 1)]
 }
+
+func longestCommonSubString(text1, text2 string) string {
+	dp := make([][]int, len(text1)+1)
+	for i := 0; i <= len(text1); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
+
+	end, maxLen := 0, 0
+	for i := 1; i <= len(text1); i++ {
+		for j := 1; j <= len(text2); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = 1 + dp[i-1][j-1]
+				end = i
+				maxLen = max(maxLen, dp[i][j])
+			} else {
+				dp[i][j] = 0
+			}
+		}
+	}
+
+	//fmt.Println(dp, end, maxLen)
+
+	return text1[end-maxLen : end]
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
--- algo/problems/0402.longest-common-substring/longest-common-substring_test.go
+++ algo/problems/0402.longest-common-substring/longest-common-substring_test.go
@@ -15,6 +15,26 @@ var qs = []struct {
 		"abcefs", "abc",
 		"abc",
 	},
+	{
+		"ddaabcefs", "abc",
+		"abc",
+	},
+	{
+		"ddaabcefs", "abcd",
+		"abc",
+	},
+	{
+		"ddaabcefs", "labcd",
+		"abc",
+	},
+	{
+		"ddaabcefs", "d",
+		"d",
+	},
+	{
+		"ddaabcefs", "z",
+		"",
+	},
 }
 
 func Test_LCS(t *testing.T) {
@@ -22,5 +42,13 @@ func Test_LCS(t *testing.T) {
 	for _, q := range qs {
 		fmt.Printf("~~%v~~\n", q)
 		ast.Equal(q.ans, maxSubTwoString(q.para1, q.para2), "输入：%v\n", q)
+	}
+}
+
+func Test_LCSV2(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, longestCommonSubString(q.para1, q.para2), "输入：%v\n", q)
 	}
 }
