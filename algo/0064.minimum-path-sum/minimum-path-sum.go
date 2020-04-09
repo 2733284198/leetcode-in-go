d48dae3721a2a68acb7b21753f6dd3ffe8d33073
aierui <aieruishi@gmail.com>
2020-04-09 23:19:07 +0800

    update 0064 minimum path sum solution

--- algo/0064.minimum-path-sum/minimum-path-sum.go
+++ algo/0064.minimum-path-sum/minimum-path-sum.go
@@ -2,4 +2,29 @@ package problem0064
 
 func minPathSum(grid [][]int) int {
 
+	row, col := len(grid), len(grid[0])
+	dp := make([][]int, row)
+
+	for i := 0; i < row; i++ {
+		dp[i] = make([]int, col)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < col; i++ {
+		dp[0][i] = grid[0][i] + dp[0][i-1]
+	}
+	for i := 1; i < row; i++ {
+		dp[i][0] = grid[i][0] + dp[i-1][0]
+	}
+
+	for i := 1; i < row; i++ {
+		for j := 1; j < col; j++ {
+			if dp[i-1][j] > dp[i][j-1] {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			}
+		}
+	}
+
+	return dp[row-1][col-1]
 }
--- algo/0064.minimum-path-sum/minimum-path-sum_test.go
+++ algo/0064.minimum-path-sum/minimum-path-sum_test.go
@@ -1,7 +1,30 @@
 package problem0064
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para [][]int
+	ans  int
+}{
+	{
+		[][]int{
+			[]int{1, 3, 1},
+			[]int{1, 5, 1},
+			[]int{4, 1, 1},
+		},
+		7,
+	},
+}
 
 func Test_MiniMumPathSum(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, minPathSum(q.para), "输入：%v\n", q)
+	}
 }
