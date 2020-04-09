afd8515d352d783e51e84ce5211885f853cd63f0
aierui <aieruishi@gmail.com>
2020-04-09 23:36:00 +0800

    add 0065 valid number problem

---
+++ algo/0065.valid-number/valid-number.go
@@ -0,0 +1,5 @@
+package problem0065
+
+func isNumber(s string) bool {
+
+}
---
+++ algo/0065.valid-number/valid-number_test.go
@@ -0,0 +1,12 @@
+package problem0065
+
+import "testing"
+
+var qs = []struct {
+	para string
+	ans  bool
+}{}
+
+func Test_IsNumber(t *testing.T) {
+
+}
