8e7b4e691dc805910e418ba41006073dccd5abfb
aierui <aieruishi@gmail.com>
2020-04-11 18:39:23 +0800

    update 0022 generate parentheses solution

--- algo/0022.generate-parentheses/generate-parentheses.go
+++ algo/0022.generate-parentheses/generate-parentheses.go
@@ -2,4 +2,25 @@ package problem0022
 
 func generateParenthesis(n int) []string {
 
+	result := make([]string, 0)
+
+	backTracking(n, n, "", &result)
+
+	return result
+}
+
+func backTracking(left, right int, tmp string, result *[]string) {
+
+	if right == 0 {
+		*result = append(*result, tmp)
+		return
+	}
+
+	if left > 0 {
+		backTracking(left-1, right, tmp+"(", result)
+	}
+
+	if right > left {
+		backTracking(left, right-1, tmp+")", result)
+	}
 }
