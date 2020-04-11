d7f02c98d04dda931186fcfd8a68ae3a35359e1e
aierui <aieruishi@gmail.com>
2020-04-11 15:15:30 +0800

    update 0025 reverse nodes in k group solution

--- algo/0025.reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
+++ algo/0025.reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
@@ -1,18 +1,42 @@
 package problem0025
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+import "github.com/aierui/leetcode-in-go/kit"
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+// ListNode Definition for singly-linked list.
+type ListNode = kit.ListNode
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
+	tail := head
+	for i := 0; i < k-1; i++ {
+		if tail == nil {
+			return head
+		}
+		tail = tail.Next
+	}
 
+	if tail == nil {
+		return head
+	}
+
+	var pre, cur, next *ListNode
+
+	pre = reverseKGroup(tail.Next, k)
+
+	tail.Next = nil
+	cur = head
+	next = cur.Next
+
+	for {
+		cur.Next = pre
+		pre = cur
+
+		if next == nil {
+			break
+		}
+
+		cur = next
+		next = cur.Next
+	}
+
+	return cur
 }
--- algo/0025.reverse-nodes-in-k-group/reverse-nodes-in-k-group_test.go
+++ algo/0025.reverse-nodes-in-k-group/reverse-nodes-in-k-group_test.go
@@ -1,7 +1,51 @@
 package problem0025
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 int
+	ans   []int
+}{
+	{
+		[]int{1, 2, 3, 4, 5},
+		1,
+		[]int{1, 2, 3, 4, 5},
+	},
+	{
+		[]int{1, 2, 3, 4, 5},
+		2,
+		[]int{2, 1, 4, 3, 5},
+	},
+	{
+		[]int{1, 2, 3, 4, 5},
+		3,
+		[]int{3, 2, 1, 4, 5},
+	},
+	{
+		[]int{1, 2, 3, 4, 5},
+		4,
+		[]int{4, 3, 2, 1, 5},
+	},
+	{
+		[]int{1, 2, 3, 4, 5},
+		5,
+		[]int{5, 4, 3, 2, 1},
+	},
+
+	// more case
+}
 
 func Test_ReverseKGroup(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, kit.List2Ints(reverseKGroup(kit.Ints2List(q.para1), q.para2)), "输入：%v\n", q)
+	}
 }
