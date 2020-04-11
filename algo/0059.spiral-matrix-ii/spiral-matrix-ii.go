4e48b2648ffc1a352b9d228a2913417dd0917b50
aierui <aieruishi@gmail.com>
2020-04-11 17:49:19 +0800

    update 0059 spiral martrix ii solution

--- algo/0059.spiral-matrix-ii/spiral-matrix-ii.go
+++ algo/0059.spiral-matrix-ii/spiral-matrix-ii.go
@@ -1,6 +1,52 @@
 package problem0059
 
 func generateMatrix(n int) [][]int {
-	var result [][]int
+	if n <= 0 {
+		return nil
+	}
 
+	var result = make([][]int, n)
+	for i := 0; i < n; i++ {
+		result[i] = make([]int, n)
+	}
+
+	num := 1
+
+	mid := n>>1 + 1
+
+	for i := 0; i < mid; i++ {
+		tmpLen := n - 2*i - 1
+
+		if tmpLen == 0 {
+			result[i][i] = num
+			continue
+		}
+
+		if tmpLen < 0 {
+			break
+		}
+
+		for j := 0; j < tmpLen; j++ {
+			result[i][i+j] = num
+			num++
+		}
+
+		for j := 0; j < tmpLen; j++ {
+			result[i+j][n-i-1] = num
+			num++
+		}
+
+		for j := tmpLen; j > 0; j-- {
+			result[n-i-1][i+j] = num
+			num++
+		}
+
+		for j := tmpLen; j > 0; j-- {
+			result[i+j][i] = num
+			num++
+		}
+
+	}
+
+	return result
 }
