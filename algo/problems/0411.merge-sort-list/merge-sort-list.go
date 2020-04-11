bf8923698896005f7b0bb8609de55398d4aaac14
aierui <aieruishi@gmail.com>
2020-04-11 13:32:54 +0800

    update 04.11 merge sort list solution

--- algo/problems/0411.merge-sort-list/merge-sort-list.go
+++ algo/problems/0411.merge-sort-list/merge-sort-list.go
@@ -1,5 +1,58 @@
 package problems0411
 
-func mergeSortLinkedList() {
+// ListNode singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
+func mergeSortLinkedList(list *ListNode) *ListNode {
+	if list == nil {
+		return nil
+	}
+
+	if list.Next == nil {
+		return list
+	}
+
+	midList := findMidList(list)
+
+	return merge(mergeSortLinkedList(list), mergeSortLinkedList(midList))
+}
+
+func merge(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{}
+	prev := head
+	for l1 != nil && l2 != nil {
+		if l1.Val > l2.Val {
+			head.Next = l2
+			l2 = l2.Next
+		} else {
+			head.Next = l1
+			l1 = l1.Next
+		}
+		head = head.Next
+	}
+
+	if l1 == nil {
+		head.Next = l2
+	} else {
+		head.Next = l1
+	}
+
+	return prev.Next
+}
+
+func findMidList(list *ListNode) *ListNode {
+	fast, slow := list, list
+
+	for fast.Next != nil && fast.Next.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	midList := slow.Next
+	slow.Next = nil
+
+	return midList
 }
--- algo/problems/0411.merge-sort-list/merge-sort-list_test.go
+++ algo/problems/0411.merge-sort-list/merge-sort-list_test.go
@@ -1,7 +1,62 @@
 package problems0411
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  []int
+}{
+	{
+		[]int{3, 2, 4, 5, 1},
+		[]int{1, 2, 3, 4, 5},
+	},
+	{
+		[]int{0, 6, 4, 5, 1},
+		[]int{0, 1, 4, 5, 6},
+	},
+}
 
 func Test_MergeSortLinkedList(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, l2s(mergeSortLinkedList(s2l(q.para))), "输入:%v", q)
+	}
+}
 
+// convert *ListNode to []int
+func l2s(head *ListNode) []int {
+	res := []int{}
+
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
+
+// convert []int to *ListNode
+func s2l(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	res := &ListNode{
+		Val: nums[0],
+	}
+	temp := res
+	for i := 1; i < len(nums); i++ {
+		temp.Next = &ListNode{
+			Val: nums[i],
+		}
+		temp = temp.Next
+	}
+
+	return res
 }
