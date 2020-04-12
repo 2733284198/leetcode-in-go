99a5e4ca9092f8e609fa221878ffb9ec3c2cc6c7
aierui <aieruishi@gmail.com>
2020-04-12 17:01:42 +0800

    update 0013 substring with concatenation of all words solution

--- algo/0013.substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words_test.go
+++ algo/0013.substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words_test.go
@@ -1,7 +1,33 @@
 package problem0013
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 string
+	para2 []string
+	ans   []int
+}{
+	{
+		"barfoothefoobarman",
+		[]string{"foo", "bar"},
+		[]int{0, 9},
+	},
+	{
+		"wordgoodgoodgoodbestword",
+		[]string{"word", "good", "best", "word"},
+		[]int{},
+	},
+}
 
 func Test_FindSubString(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, findSubstring(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
