9d19f098ccc62b6c2eda9caa1ebbc717649bb56d
aierui <aieruishi@gmail.com>
2020-04-14 00:01:58 +0800

    update 0541 reverse string ii solution

--- algo/0541.reverse-string-ii/reverse-string-ii.go
+++ algo/0541.reverse-string-ii/reverse-string-ii.go
@@ -1,7 +1,24 @@
 package problem0541
 
 func reverseStr(s string, k int) string {
+	bytes := []byte(s)
+	l := len(bytes)
+	left := 0
 
-	
+	for i := 0; left < l; i++ {
+		left = i * 2 * k
+		right := left +k - 1
 
+		if right > l - 1 {
+			right = l - 1
+		}
+
+		for left < right {
+			bytes[left], bytes[right] = bytes[right], bytes[left]
+			left ++
+			right --
+		}
+	}
+
+	return string(bytes)
 }
--- algo/0541.reverse-string-ii/reverse-string-ii_test.go
+++ algo/0541.reverse-string-ii/reverse-string-ii_test.go
@@ -1,7 +1,28 @@
 package problem0541
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 string
+	para2 int
+	ans   string
+}{
+	{
+		"abcdefg",
+		2,
+		"bacdfeg",
+	},
+}
 
 func Test_ReverseString(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, reverseStr(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
