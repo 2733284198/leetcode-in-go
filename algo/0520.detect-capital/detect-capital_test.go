d26855f292d70847d99eb7cca291c9fca98fc16a
aierui <aieruishi@gmail.com>
2020-04-15 22:52:37 +0800

    update 0520 dedtct capital solution

--- algo/0520.detect-capital/detect-capital_test.go
+++ algo/0520.detect-capital/detect-capital_test.go
@@ -1,7 +1,39 @@
 package problem0520
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para string
+	ans  bool
+}{
+	{
+		"ABC",
+		true,
+	},
+	{
+		"Abc",
+		true,
+	},
+	{
+
+		"abc",
+		true,
+	},
+	{
+		"ABBc",
+		false,
+	},
+}
 
 func Test_DetectCapitalUse(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, detectCapitalUse(q.para), "输入：%v\n", q)
+	}
 }
