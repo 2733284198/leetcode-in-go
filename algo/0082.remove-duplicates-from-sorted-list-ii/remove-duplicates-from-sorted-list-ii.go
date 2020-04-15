8ca626840201f46d618f3ed8ec3eb243f13f21e9
aierui <aieruishi@gmail.com>
2020-04-15 23:49:36 +0800

    update 0082 remove duplicates from sorted list ii solution

--- algo/0082.remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.go
+++ algo/0082.remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.go
@@ -6,5 +6,26 @@ import "github.com/aierui/leetcode-in-go/kit"
 type ListNode = kit.ListNode
 
 func deleteDuplicates(head *ListNode) *ListNode {
+	prev := &ListNode{0, head}
+	cur := prev
+	count := 0
 
+	for head != nil && head.Next != nil {
+
+		for head.Next != nil && head.Val == head.Next.Val {
+			head = head.Next
+			count++
+		}
+
+		if count > 0 {
+			cur.Next = head.Next
+			head = head.Next
+			count = 0
+		} else {
+			cur = head
+			head = head.Next
+		}
+	}
+
+	return prev.Next
 }
--- algo/0082.remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii_test.go
+++ algo/0082.remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii_test.go
@@ -1,7 +1,31 @@
 package problem0082
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  []int
+}{
+	{
+		[]int{1, 2, 3},
+		[]int{1, 2, 3},
+	},
+	{
+		[]int{1, 2, 3, 3, 4, 4, 5},
+		[]int{1, 2, 5},
+	},
+}
 
 func Test_DeleteDuplicates(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, kit.List2Ints(deleteDuplicates(kit.Ints2List(q.para))), "输入：%v\n", q)
+	}
 }
