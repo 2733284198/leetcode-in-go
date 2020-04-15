234cfb34b402bfb45f484eea41c1122a3dc447f3
aierui <aieruishi@gmail.com>
2020-04-16 00:19:10 +0800

    update 0026 remove duplicates from sorted array solution

--- algo/0026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
+++ algo/0026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -10,3 +10,14 @@ func removeDuplicates(nums []int) int {
 
 	return len(nums)
 }
+
+func removeDuplicatesV2(nums []int) int {
+	n := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[n] != nums[i] {
+			nums[n+1] = nums[i]
+			n++
+		}
+	}
+	return n + 1
+}
--- algo/0026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array_test.go
+++ algo/0026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array_test.go
@@ -29,6 +29,14 @@ func Test_RemoveDuplicates(t *testing.T) {
 	}
 }
 
+func Test_RemoveDuplicatesV2(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, removeDuplicatesV2(q.para), "输入：%v\n", q)
+	}
+}
+
 func Benchmark_RemoveDuplicates(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, q := range qs {
