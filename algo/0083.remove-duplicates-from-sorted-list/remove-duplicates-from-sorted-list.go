437fc4a137f50dbe9e131cf079bf124e495a04e3
aierui <aieruishi@gmail.com>
2020-04-15 23:20:40 +0800

    update 0083 remove duplicates from sorted list solution

--- algo/0083.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
+++ algo/0083.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -1,5 +1,38 @@
 package problem0083
 
-func deleteDuplicates(hand *ListNode) *ListNode  {
-	
+import "github.com/aierui/leetcode-in-go/kit"
+
+// ListNode
+type ListNode = kit.ListNode
+
+// func deleteDuplicates(head *ListNode) *ListNode  {
+// 	cur := head
+// 	for cur != nil {
+// 		for cur.Next != nil && cur.Next.Val == cur.Val {
+// 			cur.Next = cur.Next.Next
+// 		}
+// 		cur = cur.Next
+// 	}
+
+// 	return head
+// }
+
+func deleteDuplicates(head *ListNode) *ListNode  {
+	if head == nil {
+		return head
+	}
+
+	// 定义两个指针
+	prev, cur := head, head.Next 
+
+	for cur != nil {
+		if cur.Val == prev.Val {
+			prev.Next = cur.Next
+		} else {
+			prev = cur
+		}
+		cur = cur.Next
+	}
+
+	return head
 }
