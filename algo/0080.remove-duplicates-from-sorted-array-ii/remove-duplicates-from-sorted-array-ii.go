6de1575b23e379ebd0ec77942affbb0021a2ac06
aierui <aieruishi@gmail.com>
2020-04-16 00:13:50 +0800

    update 0080 remove duplicates from sorted array ii

--- algo/0080.remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.go
+++ algo/0080.remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.go
@@ -1,5 +1,13 @@
 package problem0080
 
 func removeDuplicates(nums []int) int {
-	
+	n := 1
+	for i := 2; i < len(nums); i++ {
+		if nums[i] != nums[n - 1] {
+			n ++
+			nums[n] = nums[i]
+		}
+	}
+
+	return n + 1
 }
--- algo/0080.remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii_test.go
+++ algo/0080.remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii_test.go
@@ -1,7 +1,30 @@
 package problem0080
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  int
+}{
+	{
+		[]int{1, 2, 3, 3, 3, 4, 4, 4},
+		6,
+	},
+	{
+		[]int{1, 2, 3, 4},
+		4,
+	},
+}
 
 func Test_RemoveDuplicates(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, removeDuplicates(q.para), "输入：%v\n", q)
+	}
 }
