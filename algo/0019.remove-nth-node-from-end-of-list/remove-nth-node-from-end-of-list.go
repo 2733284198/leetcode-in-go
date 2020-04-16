9f209683e500195673f5d6fe55e7f6bd5c8e5a79
aierui <aieruishi@gmail.com>
2020-04-16 09:23:06 +0800

    update 0019 remove nth node from end of list solution

--- algo/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
+++ algo/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -1,5 +1,25 @@
 package problem0019
 
-func removeNthFromEnd(head *ListNode, n int) *ListNode {
+import "github.com/aierui/leetcode-in-go/kit"
 
+type ListNode = kit.ListNode
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	result := &ListNode{Val: 0, Next: head} // 哨兵节点
+	var prev *ListNode
+	cur := result
+	i := 1
+
+	for head != nil {
+		if i >= n {
+			prev = cur
+			cur = cur.Next
+		}
+		head = head.Next
+		i++
+	}
+
+	prev.Next = prev.Next.Next
+
+	return result.Next
 }
--- algo/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list_test.go
+++ algo/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list_test.go
@@ -1,7 +1,34 @@
 package problem0019
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 int
+	ans   []int
+}{
+	{
+		[]int{1, 2, 3, 4, 5},
+		2,
+		[]int{1, 2, 3, 5},
+	},
+	{
+		[]int{3, 4, 1, 2, 3, 4, 5},
+		5,
+		[]int{3, 4, 2, 3, 4, 5},
+	},
+}
 
 func Test_RemoveNthNodeFromEndOfList(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, kit.List2Ints(removeNthFromEnd(kit.Ints2List(q.para1), q.para2)), "输入：%v\n", q)
+	}
 }
