468336483be7877193f2b9220f39842d47a86b46
aierui <aieruishi@gmail.com>
2020-04-18 19:58:50 +0800

    update longest increasing subquence solution

--- algo/0300.longest-incresing-subquence/longest-increasing-subquence.go
+++ algo/0300.longest-incresing-subquence/longest-increasing-subquence.go
@@ -1,5 +1,30 @@
 package problem0300
 
 func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
 
+	dp, res := make([]int, len(nums)), 0
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+	}
+
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		res = max(res, dp[i])
+	}
+
+	return res
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
 }
--- algo/0300.longest-incresing-subquence/longest-increasing-subquence_test.go
+++ algo/0300.longest-incresing-subquence/longest-increasing-subquence_test.go
@@ -1,7 +1,26 @@
 package problem0300
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  int
+}{
+	{
+		[]int{10, 9, 2, 5, 3, 7, 101, 18},
+		4,
+	},
+}
 
 func Test_LengthOfLIS(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, lengthOfLIS(q.para), "输入：%v\n", q)
+	}
 }
