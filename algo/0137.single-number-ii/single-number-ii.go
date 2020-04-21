932bf26331c29fd12617c0b86e3c77d59e28449d
aierui <aieruishi@gmail.com>
2020-04-21 17:18:20 +0800

    update 0137 single number ii solution

--- algo/0137.single-number-ii/single-number-ii.go
+++ algo/0137.single-number-ii/single-number-ii.go
@@ -1,5 +1,28 @@
 package problem0137
 
-func singleNumber(nums []int) int {
+func singleNumberHashMap(nums []int) int {
+	m := make(map[int]int)
 
+	for _, k := range nums {
+		m[k]++
+	}
+
+	for k, v := range m {
+		if v == 1 {
+			return k
+		}
+	}
+
+	return 0
+}
+
+func singleNumber(nums []int) int {
+	i, j := 0, 0
+
+	for _, v := range nums {
+		i = (i ^ v) & ^j
+		j = (j ^ v) & ^i
+	}
+
+	return i
 }
--- algo/0137.single-number-ii/single-number-ii_test.go
+++ algo/0137.single-number-ii/single-number-ii_test.go
@@ -1,7 +1,38 @@
 package problem0137
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  int
+}{
+	{
+		[]int{2, 2, 3, 2},
+		3,
+	},
+	{
+		[]int{0, 1, 0, 1, 1, 2, 0, 99, 2, 2},
+		99,
+	},
+}
+
+func Test_SingleNumberHashMap(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, singleNumberHashMap(q.para), "输入：%v\n", q)
+	}
+}
 
 func Test_SingleNumber(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, singleNumber(q.para), "输入：%v\n", q)
+	}
 }
