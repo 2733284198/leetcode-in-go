009df8fe0d17e56d1c7b0379a3beb670d190ec79
aierui <aieruishi@gmail.com>
2020-04-21 21:42:30 +0800

    update problems 0421 base62 solution

--- algo/problems/0421.base62/base62.go
+++ algo/problems/0421.base62/base62.go
@@ -1,9 +1,52 @@
 package problems0421
 
-func base62To10(s string) int {
+import (
+	"strconv"
+)
 
+func base62To10(s string) int {
+	var r int
+	for _, v := range []byte(s) {
+		i := int(getValue(rune(v)))
+		r = 62*r + int(i)
+	}
+	return r
 }
 
-func base10To62(a int) string {
+func base10To62(num int) string {
+	str := make([]byte, 0)
+	for {
+		if num <= 0 {
+			break
+		}
+		i := num % 62 // 余数
+		str = append([]byte(toChar(i)), str...)
+		num /= 62 // 商
+	}
 
+	return string(str)
+}
+
+func toChar(i int) string {
+	if i >= 0 && i <= 9 {
+		return strconv.Itoa(i)
+	}
+
+	if i > 9 && i < 36 {
+		return string('a' + i - 10)
+	}
+
+	return string('A' + i - 36)
+}
+
+func getValue(s rune) rune {
+	if s >= '0' && s <= '9' {
+		return s - '0'
+	}
+
+	if s >= 'a' && s <= 'z' {
+		return s - 'a' + 10
+	}
+
+	return s - 'A' + 36
 }
--- algo/problems/0421.base62/base62_test.go
+++ algo/problems/0421.base62/base62_test.go
@@ -1,11 +1,68 @@
 package problems0421
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs62 = []struct {
+	para string
+	ans  int
+}{
+	{
+		"A40",
+		138632,
+	},
+	{
+		"FX8",
+		161270,
+	},
+}
+
+var qs10 = []struct {
+	para int
+	ans  string
+}{
+	{
+		138632,
+		"A40",
+	},
+	{
+		161270,
+		"FX8",
+	},
+}
 
 func Test_Base62To10(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs62 {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, base62To10(q.para), "输入：%v\n", q)
+	}
+}
 
+func Benchmark_Base62To10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs62 {
+			base62To10(q.para)
+		}
+	}
 }
 
 func Test_Base10To62(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs10 {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, base10To62(q.para), "输入：%v\n", q)
+	}
+}
 
+func Benchmark_Base10To62(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs10 {
+			base10To62(q.para)
+		}
+	}
 }
