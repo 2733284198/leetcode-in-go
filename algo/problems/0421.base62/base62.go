285673b7e91522183cfd1d2b2c9261acea26d97b
aierui <aieruishi@gmail.com>
2020-04-21 18:48:45 +0800

    add problems 0421 base62 problem

---
+++ algo/problems/0421.base62/base62.go
@@ -0,0 +1,9 @@
+package problems0421
+
+func base62To10(s string) int {
+
+}
+
+func base10To62(a int) string {
+
+}
---
+++ algo/problems/0421.base62/base62_test.go
@@ -0,0 +1,11 @@
+package problems0421
+
+import "testing"
+
+func Test_Base62To10(t *testing.T) {
+
+}
+
+func Test_Base10To62(t *testing.T) {
+
+}
