84b34d665814dd3cfd5338192283804b50324809
aierui <aieruishi@gmail.com>
2020-04-21 22:16:53 +0800

    update 0043 multiply strings solution

--- algo/0043.multiply-strings/multiply-strings.go
+++ algo/0043.multiply-strings/multiply-strings.go
@@ -1,5 +1,29 @@
 package problem0043
 
-func multiply(num1, num2 string)  string {
-	
+func multiply(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+	num1Len, num2Len := len(num1), len(num2)
+	totalLen := num1Len + num2Len
+	carry := make([]byte, totalLen)
+	for i := num1Len; i > 0; i-- {
+		index := num2Len + i - 1
+		n1 := num1[i-1] - '0'
+		for j := num2Len; j > 0; j-- {
+			n := carry[index] + n1*(num2[j-1]-'0') //计算
+			carry[index] = n % 10                  //当前值
+			index--
+			carry[index] += n / 10 //进位
+		}
+	}
+
+	j := -1
+	for i := 0; i < totalLen; i++ {
+		if carry[i] != 0 && j == -1 {
+			j = i
+		}
+		carry[i] += '0'
+	}
+	return string(carry[j:])
 }
--- algo/0043.multiply-strings/multiply-strings_test.go
+++ algo/0043.multiply-strings/multiply-strings_test.go
@@ -1,7 +1,44 @@
 package problem0043
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 string
+	para2 string
+
+	ans string
+}{
+	{
+		"3",
+		"2",
+		"6",
+	},
+	{
+		"123",
+		"456",
+		"56088",
+	},
+	{
+		"1234567890",
+		"4561234567234",
+		"5631153735465142516260",
+	},
+	{
+		"12345678905632341153343273546514243516260",
+		"45612345672345631153234735465142516260",
+		"563115373803488063666544004426994190080372273560949507970698672183566624387600",
+	},
+}
 
 func Test_Multiply(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, multiply(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
