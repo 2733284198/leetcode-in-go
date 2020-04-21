ae1f4655c6d85c04ebf03d7bc7595bd9c02af255
aierui <aieruishi@gmail.com>
2020-04-21 13:35:58 +0800

    update problems 0420 array odd even adjustment solution

--- algo/problems/0420.array-odd-even-adjustment/array-odd-even-adjustment.go
+++ algo/problems/0420.array-odd-even-adjustment/array-odd-even-adjustment.go
@@ -1,5 +1,23 @@
 package problems0420
 
 func arrayAdjust(nums []int)  {
-	
+	nums2 := make([]int, len(nums))
+	copy(nums2, nums)
+	oddCount, evenCount := 0, 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] % 2 == 1 {
+			oddCount ++
+		}
+	}
+
+	for i := 0; i < len(nums2); i++ {
+		if nums2[i]%2 == 0 {
+			 nums[oddCount] = nums2[i]
+			 oddCount ++
+		} else {
+			nums[evenCount] = nums2[i]
+			evenCount ++
+		}
+	}
 }
