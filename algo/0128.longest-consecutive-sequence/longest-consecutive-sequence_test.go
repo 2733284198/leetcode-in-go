639cf5874614e2ad22de8e3e64c55b8d36840d8a
aierui <aieruishi@gmail.com>
2020-04-21 09:35:47 +0800

    add longest consecutive sequence problem

---
+++ algo/0128.longest-consecutive-sequence/longest-consecutive-sequence_test.go
@@ -0,0 +1,27 @@
+package problem0128
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  int
+}{
+	{
+		[]int{100, 4, 200, 1, 3, 2},
+		4,
+	},
+}
+
+func Test_LongestConsecutive(t *testing.T) {
+
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, longestConsecutive(q.para), "输入：%v\n", q)
+	}
+}
