798035a7935b912f4de90265df9b19c612ec4198
aierui <aieruishi@gmail.com>
2020-04-24 09:36:15 +0800

    update 0115 distinct subsequences solution

--- algo/0115.distinct-subsequences/distinct-subsequences.go
+++ algo/0115.distinct-subsequences/distinct-subsequences.go
@@ -1,5 +1,16 @@
 package problem0115
 
 func numDistinct(s, t string) int {
+	dp := make([]int, len(t)+1)
+	dp[0] = 1
 
+	for i := 1; i <= len(s); i++ {
+		for j := len(t); j >= 1; j-- {
+			if s[i-1] == t[j-1] {
+				dp[j] += dp[j-1]
+			}
+		}
+	}
+
+	return dp[len(t)]
 }
