bd61bf6033b0b1bd4ba74db32dc7d3c7b0824218
aierui <aieruishi@gmail.com>
2020-04-27 23:06:09 +0800

    add 0099 recover binary search tree problem

---
+++ algo/0099.recover-binary-search-tree/recover-binary-search-tree.go
@@ -0,0 +1,10 @@
+package problem0099
+
+import "github.com/aierui/leetcode-in-go/kit"
+
+// TreeNode
+type TreeNode = kit.TreeNode
+
+func recoverTree(root *TreeNode) {
+
+}
---
+++ algo/0099.recover-binary-search-tree/recover-binary-search-tree_test.go
@@ -0,0 +1,30 @@
+package problem0099
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 []int
+	ans   []int
+}{
+	{
+		[]int{3, 2, 1},
+		[]int{3, 2, 1},
+		[]int{1, 2, 3},
+	},
+}
+
+func Test_RecoverTree(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, kit.PreIn2Tree(q.para1, q.para2), "输入：%v\n", q)
+	}
+}
