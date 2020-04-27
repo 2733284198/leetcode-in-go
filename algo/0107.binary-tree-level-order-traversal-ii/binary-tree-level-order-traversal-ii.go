5605d92e0e350163ff48de132275be60e41420b6
aierui <aieruishi@gmail.com>
2020-04-28 00:13:15 +0800

    update 0107 binary tree level order traversal solution

--- algo/0107.binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.go
+++ algo/0107.binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.go
@@ -4,6 +4,29 @@ import "github.com/aierui/leetcode-in-go/kit"
 
 type TreeNode = kit.TreeNode
 
-func levelOrderBottom(root *TreeNode) [][]int {
+var result [][]int
 
+func levelOrderBottom(root *TreeNode) [][]int {
+	result = [][]int{}
+	dfs(root, 0)
+
+	// reverse array
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	return result
+}
+
+func dfs(root *TreeNode, level int) {
+	if root != nil {
+		if len(result) == level {
+			result = append(result, []int{})
+		}
+
+		result[level] = append(result[level], root.Val)
+
+		dfs(root.Left, level+1)
+		dfs(root.Right, level+1)
+	}
 }
