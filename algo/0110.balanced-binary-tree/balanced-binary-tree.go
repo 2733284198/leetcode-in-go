67eeb5dff2697d7a19329613d96dcc159002da8e
aierui <aieruishi@gmail.com>
2020-04-28 08:35:38 +0800

    add 0110 balanced binary tree problems

---
+++ algo/0110.balanced-binary-tree/balanced-binary-tree.go
@@ -0,0 +1,5 @@
+package problem0110
+
+func isBalanced(root *TreeNode) bool {
+
+}
---
+++ algo/0110.balanced-binary-tree/balanced-binary-tree_test.go
@@ -0,0 +1,33 @@
+package problem0110
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 []int
+	ans   bool
+}{
+	{
+		[]int{3, 9, 20, 15, 7},
+		[]int{9, 3, 15, 20, 7},
+		true,
+	},
+	{
+		[]int{1, 2, 3, 4, 4, 3, 2},
+		[]int{4, 3, 4, 2, 3, 1, 2},
+		false,
+	},
+}
+
+func Test_IsBalanced(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, isBalanced(q.para1, q.para2), "输入：%v\n", q)
+	}
+}
