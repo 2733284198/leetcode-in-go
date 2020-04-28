e7f865f4bc735c95ed9708e670139f52e8bd98a9
aierui <aieruishi@gmail.com>
2020-04-29 00:17:54 +0800

    update 0226 invert binary tree solution

--- algo/0226.invert-binary-tree/invert-binary-tree.go
+++ algo/0226.invert-binary-tree/invert-binary-tree.go
@@ -5,5 +5,13 @@ import "github.com/aierui/leetcode-in-go/kit"
 type TreeNode = kit.TreeNode
 
 func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
 
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
 }
--- algo/0226.invert-binary-tree/invert-binary-tree_test.go
+++ algo/0226.invert-binary-tree/invert-binary-tree_test.go
@@ -1,7 +1,32 @@
 package problem0226
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 []int
+	ans1  []int
+	ans2  []int
+}{
+	{
+		[]int{4, 2, 1, 3, 7, 6, 9},
+		[]int{1, 2, 3, 4, 5, 7, 9},
+		[]int{4, 7, 9, 6, 2, 3, 1},
+		[]int{9, 7, 6, 4, 3, 2, 1},
+	},
+}
 
 func Test_InvertTree(t *testing.T) {
+	ast := assert.New(t)
 
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(kit.PreIn2Tree(q.ans1, q.ans2), invertTree(kit.PreIn2Tree(q.para1, q.para2)))
+	}
 }
