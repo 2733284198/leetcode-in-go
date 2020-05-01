e3d5b84491ec83982976851fed728c455e74e235
aierui <aieruishi@gmail.com>
2020-05-01 09:44:11 +0800

    update 0095 unique binary search tree ii solution

--- algo/0095.unique-binary-search-tree-ii/unique-binary-search-tree-ii.go
+++ algo/0095.unique-binary-search-tree-ii/unique-binary-search-tree-ii.go
@@ -2,8 +2,42 @@ package problem0095
 
 import "github.com/aierui/leetcode-in-go/kit"
 
-type TreeNode = kit.ListNode
+type TreeNode = kit.TreeNode
 
 func generateTrees(n int) []*TreeNode {
+	if n == 0 {
+		return nil
+	}
 
+	return doGenerateTrees(1, n)
+}
+
+func doGenerateTrees(left, right int) []*TreeNode {
+	if left > right {
+		return []*TreeNode{nil}
+	}
+
+	if left == right {
+		return []*TreeNode{&TreeNode{Val: left}}
+	}
+
+	trees := make([]*TreeNode, 0, right-left+1)
+
+	for i := left; i <= right; i++ {
+		leftTrees := doGenerateTrees(left, i-1)
+		rightTrees := doGenerateTrees(i+1, right)
+
+		for m := 0; m < len(leftTrees); m++ {
+			for n := 0; n < len(rightTrees); n++ {
+				tree := &TreeNode{Val: i}
+
+				tree.Left = leftTrees[m]
+				tree.Right = rightTrees[n]
+
+				trees = append(trees, tree)
+			}
+		}
+	}
+
+	return trees
 }
--- algo/0095.unique-binary-search-tree-ii/unique-binary-search-tree-ii_test.go
+++ algo/0095.unique-binary-search-tree-ii/unique-binary-search-tree-ii_test.go
@@ -2,6 +2,10 @@ package problem0095
 
 import "testing"
 
+var qs = []struct {
+	para int
+}{}
+
 func Test_GenerateTrees(t *testing.T) {
 
 }
