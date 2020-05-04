6e323bf744a4925aae262ffc776cfe45d66ee5de
aierui <aieruishi@gmail.com>
2020-05-04 13:13:42 +0800

    update 209 minimum size subarray sum solution

--- algo/0209.minimum-size-subarray-sum/minimum-size-subarray-sum.go
+++ algo/0209.minimum-size-subarray-sum/minimum-size-subarray-sum.go
@@ -1,5 +1,35 @@
 package problem0209
 
 func minSubArrayLen(s int, nums []int) int  {
-	
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+	// 定义滑动窗口的开始、结束指针 i、j
+	i,j,sum := 0,0, nums[0]
+	minLen := len(nums) + 1
+	// 窗口大小就是要求的数组长度 一直维持滑动窗口的和 >= s
+	// 如果小了 右边加元素 j ++
+	// 如果大了 左边减元素 i ++
+	for j < len(nums) {
+		if sum >= s {
+			if j - i +1 < minLen {
+				minLen = j - i + 1
+			}
+			sum -= nums[i]
+			i ++
+		} else {
+			j ++
+			if j >= len(nums) {
+				break
+			}
+			sum += nums[j]
+		}
+	}
+
+	if minLen == len(nums) + 1 {
+		return 0
+	}
+
+	return minLen
 }
--- algo/0209.minimum-size-subarray-sum/minimum-size-subarray-sum_test.go
+++ algo/0209.minimum-size-subarray-sum/minimum-size-subarray-sum_test.go
@@ -2,8 +2,29 @@ package problem0209
 
 import (
 	"testing"
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
 )
 
+
+var qs =[]struct {
+	para1 int
+	para2 []int
+	ans int
+} {
+	{
+		7,
+		[]int{2,3,1,2,4,3},
+		2,
+	},
+}
+
 func Test_MinSubArrayLen(t *testing.T) {
-	
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, minSubArrayLen(q.para1, q.para2), "输入：%v\n", q)
+	}
+
 }
