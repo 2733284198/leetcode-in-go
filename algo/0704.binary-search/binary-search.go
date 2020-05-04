5a0f24e2b41d1dec6158300ad9a84a93b1a7c6c1
aierui <aieruishi@gmail.com>
2020-05-04 13:40:39 +0800

    update 704 binary search solution

--- algo/0704.binary-search/binary-search.go
+++ algo/0704.binary-search/binary-search.go
@@ -1,5 +1,23 @@
 package problem0704
 
 func search(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
 
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if target > nums[mid] {
+			left = mid + 1
+		} else if target < nums[mid] {
+			right = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	return -1
 }
--- algo/0704.binary-search/binary-search_test.go
+++ algo/0704.binary-search/binary-search_test.go
@@ -17,6 +17,11 @@ var qs = []struct {
 		4,
 		3,
 	},
+	{
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		10,
+		-1,
+	},
 }
 
 func Test_Search(t *testing.T) {
@@ -25,5 +30,13 @@ func Test_Search(t *testing.T) {
 	for _, q := range qs {
 		fmt.Printf("~~%v~~\n", q)
 		ast.Equal(q.ans, search(q.para1, q.para2), "输入：%v\n", q)
+	}
+}
+
+func Benchmark_Search(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs {
+			search(q.para1, q.para2)
+		}
 	}
 }
