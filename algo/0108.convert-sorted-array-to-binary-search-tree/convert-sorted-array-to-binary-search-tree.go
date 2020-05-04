281323ace4e1214a14633a03f0abef7fa1cb431b
aierui <aieruishi@gmail.com>
2020-05-04 12:26:01 +0800

    update 108 convert sorted array to binary search tree solution

--- algo/0108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.go
+++ algo/0108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.go
@@ -8,4 +8,20 @@ type TreeNode = kit.TreeNode
 
 func sortedArrayToBST(nums []int) *TreeNode {
 
+	if nums == nil || len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+
+	left := nums[:mid]
+	right := nums[mid+1:]
+
+	node := &TreeNode{
+		nums[mid],
+		sortedArrayToBST(left),
+		sortedArrayToBST(right),
+	}
+
+	return node
 }
--- algo/0108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree_test.go
+++ algo/0108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree_test.go
@@ -1,5 +1,32 @@
 package problem0108
 
+
+import (
+	"testing"
+	"fmt"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans []int
+} {
+	{
+		[]int{1, 2, 3, 4, 5, 6, 7},
+		[]int{4, 2, 1, 3, 6, 5, 7},
+	},
+	{
+		[]int{},
+		nil,
+	},
+}
+
 func Test_SortedArrayToBST(t *testing.T) {
-	
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, kit.Tree2Preorder(sortedArrayToBST(q.para)), "输入：%v\n", q)
+	}
 }
