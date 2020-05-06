074df022991778a1a0302e7a052d234a0293669b
aierui <aieruishi@gmail.com>
2020-05-06 23:45:50 +0800

    update sort test unit

--- kit/Sort_test.go
+++ kit/Sort_test.go
@@ -33,3 +33,54 @@ func Benchmark_Bubble(b *testing.B) {
 		}
 	}
 }
+
+func Test_Insert(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		Insert(q.para)
+		ast.Equal(q.ans, q.para, "输入：%v\n", q)
+	}
+}
+
+func Benchmark_Insert(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs {
+			Insert(q.para)
+		}
+	}
+}
+
+func Test_Selection(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		Selection(q.para)
+		ast.Equal(q.ans, q.para, "输入：%v\n", q)
+	}
+}
+
+func Benchmark_Selection(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs {
+			Selection(q.para)
+		}
+	}
+}
+
+func Test_Quick(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		Quick(q.para)
+		ast.Equal(q.ans, q.para, "输入：%v\n", q)
+	}
+}
+
+func Benchmark_Quick(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs {
+			Quick(q.para)
+		}
+	}
+}
