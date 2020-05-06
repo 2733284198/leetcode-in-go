95465775d6ad2ac8918011fe6c115e737956f2e8
aierui <aieruishi@gmail.com>
2020-05-06 23:42:52 +0800

    update sort test unit

---
+++ kit/Sort_test.go
@@ -0,0 +1,35 @@
+package kit
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para []int
+	ans  []int
+}{
+	{
+		[]int{2, 3, 32, 3, 4, 1, 7, 10},
+		[]int{1, 2, 3, 3, 4, 7, 10, 32},
+	},
+}
+
+func Test_Bubble(t *testing.T) {
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		Bubble(q.para)
+		ast.Equal(q.ans, q.para, "输入：%v\n", q)
+	}
+}
+
+func Benchmark_Bubble(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range qs {
+			Bubble(q.para)
+		}
+	}
+}
