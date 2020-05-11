93911f7ade8ac4cfe1251d274eaae9bae23699bf
aierui <aieruishi@gmail.com>
2020-05-11 22:56:43 +0800

    add 450 delete node in a bst

---
+++ algo/0450.delete-node-in-a-bst/delete-node-in-a-bst.go
@@ -0,0 +1,9 @@
+package problem0450
+
+import "github.com/aierui/leetcode-in-go/kit"
+
+type TreeNode = kit.TreeNode
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+
+}
---
+++ algo/0450.delete-node-in-a-bst/delete-node-in-a-bst_test.go
@@ -0,0 +1,7 @@
+package problem0450
+
+import "testing"
+
+func Test_DeleteNode(t *testing.T) {
+
+}
