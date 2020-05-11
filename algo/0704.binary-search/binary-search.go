8d0c62bc6ebaac9d246604e416fe28e91481a375
aierui <aieruishi@gmail.com>
2020-05-11 14:18:43 +0800

    update 0704 binary search solution

--- algo/0704.binary-search/binary-search.go
+++ algo/0704.binary-search/binary-search.go
@@ -8,8 +8,8 @@ func search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
 
 	for left <= right {
-		mid := left + (right-left)/2
-
+		// mid := left + (right-left)/2
+		mid := left + (right-left)>>1
 		if target > nums[mid] {
 			left = mid + 1
 		} else if target < nums[mid] {
