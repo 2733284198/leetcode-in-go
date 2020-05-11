72d7363c03757af7a87371c15b1454d324c7f0a9
aierui <aieruishi@gmail.com>
2020-05-11 22:35:02 +0800

    update 0701 insert into a binary search tree solution

--- algo/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
+++ algo/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
@@ -5,6 +5,7 @@ import "github.com/aierui/leetcode-in-go/kit"
 type TreeNode = kit.TreeNode
 
 // Time: O(H) H 指树的高度，avg O(Log(N)), worst O(N)
+// Space: avg O(H) worst O(N)
 // insertIntoBST by recursion
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
 	if root == nil {
