d9410eaa7826063f443c447c0b92bbf9870f4c12
aierui <aieruishi@gmail.com>
2020-05-11 22:32:10 +0800

    update 0701 insert into a binary search tree solution

--- algo/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
+++ algo/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
@@ -4,6 +4,18 @@ import "github.com/aierui/leetcode-in-go/kit"
 
 type TreeNode = kit.TreeNode
 
+// Time: O(H) H 指树的高度，avg O(Log(N)), worst O(N)
+// insertIntoBST by recursion
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
 
+	if val > root.Val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+
+	return root
 }
--- algo/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree_test.go
+++ algo/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree_test.go
@@ -1,7 +1,42 @@
 package problem0701
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 []int
+	para3 int
+	ans1  []int
+	ans2  []int
+}{
+	{
+		[]int{2, 1, 3},
+		[]int{1, 2, 3},
+		4,
+		[]int{2, 1, 3, 4},
+		[]int{1, 2, 3, 4},
+	},
+	{
+		[]int{4, 2, 1, 3, 7},
+		[]int{1, 2, 3, 4, 7},
+		5,
+		[]int{4, 2, 1, 3, 7, 5},
+		[]int{1, 2, 3, 4, 5, 7},
+	},
+}
 
 func Test_InsertIntoBST(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		root := kit.PreIn2Tree(q.para1, q.para2)
+		tree := kit.PreIn2Tree(q.ans1, q.ans2)
+		ast.Equal(tree, insertIntoBST(root, q.para3), "输入：%v\n", q)
+	}
 }
