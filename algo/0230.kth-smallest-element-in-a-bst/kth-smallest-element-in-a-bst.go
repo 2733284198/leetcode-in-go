1483d6329a05787ee54ed46e4a856bd014b02a9d
aierui <aieruishi@gmail.com>
2020-05-11 21:47:06 +0800

    update 0230 kth smallest element in a bst solution

--- algo/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.go
+++ algo/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.go
@@ -1,5 +1,33 @@
 package problem0230
 
-func kthSmallest(root *TreeNode, k int) int {
+import (
+	"fmt"
 
+	"github.com/aierui/leetcode-in-go/kit"
+)
+
+type TreeNode = kit.TreeNode
+
+func kthSmallest(root *TreeNode, k int) int {
+	nums := tree2Inorder(root)
+
+	fmt.Println(nums)
+
+	return nums[k-1]
+}
+
+func tree2Inorder(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Val}
+	}
+
+	result := tree2Inorder(root.Left)
+	result = append(result, root.Val)
+	result = append(result, tree2Inorder(root.Right)...)
+
+	return result
 }
--- algo/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst_test.go
+++ algo/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst_test.go
@@ -1,7 +1,38 @@
 package problem0230
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 []int
+	para3 int
+
+	ans int
+}{
+	{
+		[]int{2, 1, 3},
+		[]int{1, 2, 3},
+		1,
+		1,
+	},
+	{
+		[]int{3, 2, 1, 4, 5},
+		[]int{1, 2, 3, 4, 5},
+		3,
+		3,
+	},
+}
 
 func Test_KthSmallest(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, kthSmallest(kit.PreIn2Tree(q.para1, q.para2), q.para3), "输入：%v\n", q)
+	}
 }
