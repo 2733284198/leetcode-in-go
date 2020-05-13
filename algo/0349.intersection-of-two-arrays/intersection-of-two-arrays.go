dd628fd0f37adb1c05fd5dec9836619337e404a3
aierui <aieruishi@gmail.com>
2020-05-13 23:31:31 +0800

    add 0349 intersection of two array solution

--- algo/0349.intersection-of-two-arrays/intersection-of-two-arrays.go
+++ algo/0349.intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -2,4 +2,19 @@ package problem0349
 
 func intersection(nums1 []int, nums2 []int) []int {
 
+	hash := make(map[int]bool)
+	result := make([]int, 0)
+
+	for _, v := range nums1 {
+		hash[v] = true
+	}
+
+	for _, v := range nums2 {
+		if hash[v] == true {
+			result = append(result, v)
+			hash[v] = false
+		}
+	}
+
+	return result
 }
--- algo/0349.intersection-of-two-arrays/intersection-of-two-arrays_test.go
+++ algo/0349.intersection-of-two-arrays/intersection-of-two-arrays_test.go
@@ -1,7 +1,33 @@
 package problem0349
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1 []int
+	para2 []int
+	ans   []int
+}{
+	{
+		[]int{1, 2, 2, 1},
+		[]int{2, 2},
+		[]int{2},
+	},
+	{
+		[]int{4, 9, 5},
+		[]int{9, 4, 9, 8, 4},
+		[]int{9, 4},
+	},
+}
 
 func Test_Intersection(t *testing.T) {
-
+	ast := assert.New(t)
+	for _, q := range qs {
+		fmt.Printf("~~%v~~\n", q)
+		ast.Equal(q.ans, intersection(q.para1, q.para2), "输入：%v\n", q)
+	}
 }
