f80cd5c89ec51205cecc61392350e9cc9cf59098
aierui <aieruishi@gmail.com>
2020-05-13 22:54:26 +0800

    update 0235 palindrome linked list solution

--- algo/0234.palindrome-linked-list/palindrome-linked-list.go
+++ algo/0234.palindrome-linked-list/palindrome-linked-list.go
@@ -7,6 +7,35 @@ import (
 type ListNode = kit.ListNode
 
 func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
 
-	return false
+	// 1、找链表的中点
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	var prev *ListNode
+
+	// 2、反转链表的尾部
+	for slow != nil {
+		temp := slow.Next
+		slow.Next = prev
+		prev = slow
+		slow = temp
+	}
+
+	// 3、遍历尾部和头部对比
+	for prev != nil {
+		if prev.Val != head.Val {
+			return false
+		}
+		prev = prev.Next
+		head = head.Next
+	}
+
+	return true
 }
