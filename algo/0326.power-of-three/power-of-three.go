b8fe6b424681c6498fc595b6f75af92968210c4b
aierui <aieruishi@gmail.com>
2020-05-13 23:02:22 +0800

    add 0326 power of three problem

---
+++ algo/0326.power-of-three/power-of-three.go
@@ -0,0 +1,5 @@
+package problem0326
+
+func isPowerOfThree(n int) bool {
+	
+}
---
+++ algo/0326.power-of-three/power-of-three_test.go
@@ -0,0 +1,30 @@
+package problem0326
+
+import "testing"
+
+
+var qs =[]struct {
+	para int
+	ans bool
+} {
+	{
+		27,
+		true,
+	},
+	{
+		0,
+		false,
+	},
+	{
+		9,
+		true,
+	},
+	{
+		45,
+		false,
+	}
+}
+
+func Test_IsPowerOfThree(t *testing.T) {
+
+}
