8288c8731c3fbd195407159b72263e890849b4bc
aierui <aieruishi@gmail.com>
2020-05-14 23:40:37 +0800

    update 0235 lowest common ancestor of a binary search tree solution

--- algo/0235.lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
+++ algo/0235.lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -5,5 +5,19 @@ import "github.com/aierui/leetcode-in-go/kit"
 type TreeNode = kit.TreeNode
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	pVal, qVal := p.Val, q.Val
+	node := root
+	for node != nil {
+		parentVal := node.Val
 
+		switch {
+		case pVal > parentVal && qVal > parentVal:
+			node = node.Right
+		case pVal < parentVal && qVal < parentVal:
+			node = node.Left
+		default:
+			return node
+		}
+	}
+	return nil
 }
--- algo/0235.lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree_test.go
+++ algo/0235.lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree_test.go
@@ -1,7 +1,40 @@
 package problem0235
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/aierui/leetcode-in-go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+var qs = []struct {
+	para1, para2 []int
+	p, q, ans    int
+}{
+	{
+		[]int{6, 2, 0, 4, 3, 5, 8, 7, 9},
+		[]int{0, 2, 3, 4, 5, 6, 7, 8, 9},
+		2,
+		8,
+		6,
+	},
+}
 
 func Test_LowestCommonAncestor(t *testing.T) {
+	ast := assert.New(t)
+	for _, item := range qs {
+		root := kit.PreIn2Tree(item.para1, item.para2)
 
+		p := &TreeNode{
+			Val: item.p,
+		}
+
+		q := &TreeNode{
+			Val: item.q,
+		}
+
+		node := lowestCommonAncestor(root, p, q)
+
+		ast.Equal(item.ans, node.Val)
+	}
 }
